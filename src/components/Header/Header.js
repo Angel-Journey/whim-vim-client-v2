22c2ab8240c053db5d69e874807dd549fc5393c8
Angel-Journey <angel.journey7@gmail.com>
2021-07-22 15:37:13 -0500

    Add site title info and header design
    Replace 'Token Token' with 'Bearer' in auth.js
    Add site title to index.html
    Add site title and design to navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import './Header.scss'
 
 const authenticatedOptions = (
   <Fragment>
@@ -25,11 +26,11 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      react-auth-template
+      <p className="logo">Whim Vim</p>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto">
+      <Nav className="ml-auto nav">
         { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
         { alwaysOptions }
         { user ? authenticatedOptions : unauthenticatedOptions }
